b320325f848d660bc26800aa1534bd5a67691373
stephanhuewe <sh@huewe.info>
2020-09-20 17:16:30 +0200

    Adding entities

---
+++ src/Controller/PostController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/post", name="post.")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="post")
+     */
+    public function index()
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="post")
+     */
+    public function create(Request $request)
+    {
+        // create a new post
+        $post = new Post();
+
+        $post->setTitle('This is a title');
+
+        // return a response
+
+    }
+}
